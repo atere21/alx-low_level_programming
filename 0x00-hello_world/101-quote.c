08c8496ff0342f8e9be4808960bd8a26804ccdeb
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-08 14:05:45 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 61);
 
 return (1);
 }
