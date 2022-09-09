7ede9985f4ad1af889273b33d8ef59f4c57b2e33
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-09 16:02:40 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + 0);
-putchar(n);
-return (0);
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
