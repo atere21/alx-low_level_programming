3809763f818c85f263a6f9a530fca9f66c9e928c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-09 17:14:55 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - program that prints all possible different combinations of two digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: 0
  */
+
 int main(void)
 {
-	int d;
-
-	for (d = 0; d < 90; d++)
-	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int n1 = 48;
+int n2;
+int com = 44;
+while (n1 <= 57)
+{
+n2 = n1 + 1;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
 }
