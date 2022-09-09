53ec07ad808455729869292fa5b5abfe0a6f10b2
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-09 13:24:17 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,26 @@
 include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Prints if number is positive, zero or negative
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
  *
- * Return: Always (Success)
- */
+ * Return: Always 0.
+*/
 int main(void)
 {
-0-positive_or_negative.c int n;
+	int n;
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-0-positive_or_negative.c if (n > 0)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is positive\n", n);
-0-positive_or_negative.c }
-0-positive_or_negative.c else if (n == 0)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is zero\n", n);
-0-positive_or_negative.c }
-0-positive_or_negative.c else
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("%d is negative\n", n);
-0-positive_or_negative.c }
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 
-0-positive_or_negative.c return (0);
+	return (0);
 }
