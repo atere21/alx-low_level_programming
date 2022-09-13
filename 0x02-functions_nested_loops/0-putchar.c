c15e0da315a27f99a0764f31f0a7def88c16e56d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 15:22:58 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	putchar('_');
-        putchar('p');
-        putchar('u');
-        putchar('t');
-        putchar('c');
-        putchar('h');
-        putchar('a');
-        putchar('r');
-        putchar('\n');
-
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
