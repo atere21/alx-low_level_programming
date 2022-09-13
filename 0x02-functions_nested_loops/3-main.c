4b0d10d7ad113f42b3529323dc668e61f996a68c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 21:20:47 +0100

    Update 3-main.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,11 +9,11 @@ int main(void)
 	int r;
 
 	r = _islower('H');
-	_putchar(r + '0');
+	putchar(r + '0');
 	r = _islower('o');
-	_putchar(r + '0');
+	putchar(r + '0');
 	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(r + '0');
+	putchar('\n');
 	return (0);
 }
