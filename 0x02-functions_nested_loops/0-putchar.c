96a70a94b766312166e0560c52c53c8a73edb416
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 12:33:29 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	char *sh = "_putchar";
+	char c[] = "_putchar";
+	int i;
 
-	while (*sh)
+	for (i = 0; i < 9; i++)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(c[i]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
