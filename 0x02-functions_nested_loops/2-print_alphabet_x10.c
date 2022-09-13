3569bdea135ce0c4f41afda813fd4498cbc49f6c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 20:34:37 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
   */
 void print_alphabet_x10(void)
 {
-	int n, co;
-	co = 0;
-	while (co < 10)
-	{
-		for (n = 'a'; n <= 'z'; n++)
-		{
-			putchar(n);
-		}
-		co++;
-		putchar('\n');
+int n, co;
+co = 0;
+while (co < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+co++;
+putchar('\n');
 	}
 }
