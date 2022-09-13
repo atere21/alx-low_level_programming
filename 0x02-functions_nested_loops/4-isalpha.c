1754defca77d32d323db57b8403a8ab2df5620ec
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 22:18:52 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,10 @@
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
 		return (1);
-	else
-		return (0);
+	}
+
+	return (0);
 }
