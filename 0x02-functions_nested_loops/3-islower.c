38411a8dc5111f8828969aa0f848394fa040644c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-13 21:13:36 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,6 +5,13 @@
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
+
+int r;
+
+	r = _islower(n);
+	_putchar(r + '0');
+	_putchar('\n');
+
 {
 	if (c >= 97 && c <= 122)
 	{
