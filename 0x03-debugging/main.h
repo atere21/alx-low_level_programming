ff768c8082595f49c339d350073f8fbf1ac6c678
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 19:01:45 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,4 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
-<<<<<<< HEAD
-#endif /* MAIN_H */
-int largest_number(int a, int b, int c)
-=======
-
->>>>>>> c9050437426b94d2ca077faee1a7cbc3c36132fc
+int largest_number(int a, int b, int c);
