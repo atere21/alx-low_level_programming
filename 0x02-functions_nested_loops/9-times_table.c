1173c1fa3b6076f6ba9398a6a36b433f730e06e4
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 08:23:20 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,32 @@
  */
 void times_table(void)
 {
-	int x, y, product;
-	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++)
-		{
-	           product = x * y;
-			if (y != 0)
-			{
-				putchar(', ');
-				putchar(' ');
-			}
-			if (y == 0)
-			{
-				putchar('0');
-			}
-			else if (product >= 10)
-			{
-				putchar((product / 10) + '0');
-				putchar((product % 10) + '0');
-			}
-			else if ((product < 10) && (y != 0))
-			{
-				putchar(' ');
-				putchar((product % 10) + '0');
-			}
-		}
-		putchar('\n');
-	}
+int x, y, product;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+product = x * y;
+if (y != 0)
+{
+putchar(',');
+putchar(' ');
+}
+if (y == 0)
+{
+putchar('0');
+}
+else if (product >= 10)
+{
+putchar((product / 10) + '0');
+putchar((product % 10) + '0');
+}
+else if ((product < 10) && (y != 0))
+{
+putchar(' ');
+putchar((product % 10) + '0');
+}
+}
+putchar('\n');
+}
 }
