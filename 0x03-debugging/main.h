49371c4406466711bdf8cd4b0693c595c54c2687
atere21 <tosinateret@gmail.com>
2022-09-14 11:08:10 -0700

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,3 +8,5 @@ int largest_number(int a, int b, int c)
 =======
 
 >>>>>>> c9050437426b94d2ca077faee1a7cbc3c36132fc
+int convert_day(int month, int day)
+void print_remaining_days(int month, int day, int year)
