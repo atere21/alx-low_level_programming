cb9b1d1248ce1a8c3da636aaa5cb5dc0166b323d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 07:46:30 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,42 +8,43 @@
  */
 void print_times_table(int n)
 {
-int a, b, c, d, e, f;
-if (n >= 0 && n < 15)
-{
-for (a = 0; a <=  n; a++)
-{
-for (b = 0; b <= n; b++)
-{
-c = a * b; d = c / 100; e = (c / 10) % 10;
-f = (c % 100) % 10;
-if (b == 0)
-{
-putchar('0');
-}
-else if (c < 10)
-{
-putchar(' '); putchar(' ');
-putchar('0' + f);
-}
-else if (c < 100)
-{
-putchar(' ');
-putchar('0' + e); putchar('0' + f);
-}
-else
-{
-putchar('0' + d);
-putchar('0' + e);
-putchar('0' + f);
-}
-if (b < n)
-{
-putchar(','); putchar(' ');
-}
-else
-putchar('\n');
-}
-}
-}
+	int a, b, c, d, e, f;
+
+	if (n >= 0 && n < 15)
+	{
+		for (a = 0; a <=  n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b; d = c / 100; e = (c /  10) % 10;
+				f = (c % 100) % 10;
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+				else if (c < 10)
+				{
+					_putchar(' '); _putchar(' ');
+					_putchar('0' + f);
+				}
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + e); _putchar('0' + f);
+				}
+				else
+				{
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
+				}
+				if (b < n)
+				{
+					_putchar(','); _putchar(' ');
+				}
+				else
+					_putchar('\n');
+			}
+		}
+	}
 }
