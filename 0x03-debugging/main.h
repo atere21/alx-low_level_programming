1893974106fabcca0ad167509c7eb4c7d5e5d093
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 19:18:46 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-#endif /* MAIN_H */
-int largest_number(int a, int b, int c)
-=======
-
->>>>>>> c9050437426b94d2ca077faee1a7cbc3c36132fc
-int convert_day(int month, int day)
-void print_remaining_days(int month, int day, int year)
-=======
 int largest_number(int a, int b, int c);
->>>>>>> ff768c8082595f49c339d350073f8fbf1ac6c678
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
