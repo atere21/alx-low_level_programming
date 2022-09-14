263dce6b22b73d953241023eb3463125b916acd5
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 08:04:58 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,7 @@
  *
  * Return: void
  */
+
 void print_times_table(int n)
 {
 int a, b, c, d, e, f;
@@ -18,20 +19,25 @@ for (b = 0; b <= n; b++)
 c = a * b; d = c / 100; e = (c / 10) % 10;
 f = (c % 100) % 10;
 if (b == 0)
+
 {
 putchar('0');
 }
 else if (c < 10)
+
 {
 putchar(' '); putchar(' ');
 putchar('0' + f);
 }
+
 else if (c < 100)
+
 {
 putchar(' ');
 putchar('0' + e); putchar('0' + f);
 }
 else
+
 {
 putchar('0' + d);
 putchar('0' + e);
