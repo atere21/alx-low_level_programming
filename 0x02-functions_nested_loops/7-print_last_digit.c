a7d785c71e259fbdb2b49ba54c0300d1e9d66093
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 11:27:41 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,12 @@
   */
 int print_last_digit(int n)
 {
-	int last;
-
-	last = n % 10;
-
-	if (last < 0)
-	{
-		last = last * -1;
-	}
-
-	putchar(last + '0');
-
-	return (last);
+int last;
+last = n % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+putchar(last + '0');
+return (last);
 }
