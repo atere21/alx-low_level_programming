fcee63251031ebec19551c72d9decc44bffbeb52
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 11:16:09 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * print_last_digit - prints last digit of integer input
  * @n: integer to modify
@@ -10,7 +10,7 @@ int print_last_digit(int n)
 	int l, p; 
 	l = n % 10;
 	if (l <  0)
-		l = -l;
+	l = -l;
 	p = '0' + l;
 	putchar(p);
 	return (l);
