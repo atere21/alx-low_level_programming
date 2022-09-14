5da97dcd83d4521e3805eb093d8a8f6ce41558ad
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 17:21:16 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_times_table - prints the times table up to the integer input
@@ -10,7 +9,7 @@ void print_times_table(int n)
 int a, b, c, d, e, f;
 if (n >= 0 && n < 15)
 {
-for (a = 0; a <=  n; a++)
+for (a = 0; a <= n; a++)
 {
 for (b = 0; b <= n; b++)
 {
