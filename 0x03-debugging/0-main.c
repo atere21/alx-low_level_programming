62687a6e4ba32d2da945902b07d0429873d46151
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-14 18:42:33 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -9,6 +8,8 @@ int main(void)
 {
 int i;
 i = 98;
+
 positive_or_negative(i);
 return (0);
+
 }
