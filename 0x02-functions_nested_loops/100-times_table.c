5e0929d53ebfc2064873968334814bcf1879844d
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 05:06:32 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
-* print_times_table - print times tables from zero to n
-* @n: integer
-*/
+ * print_times_table - prints the times table up to the integer input
+ * @n: integer input
+ *
+ * Return: void
+ */
 
 void print_times_table(int n)
 {
-int a = 0;
+	int a, b, c, d, e, f;
 
-if (n >= 0 && n <= 15)
-{
-while (a <= n)
-{
-int b = 1;
-
-putchar(0);
-while (b <= n)
-{
-int p = a * b;
-
-putchar(,);
-putchar( );
-if (p < 10)
-{
-putchar( );
-putchar( );
-putchar(p + 0);
-}
-else if (p > 99)
-{
-putchar((p / 100) + 0);
-putchar(((p / 10) % 10) + 0);
-putchar((p % 10) + 0);
-}
-else
-{
-putchar( );
-putchar((p / 10) + 0);
-putchar((p % 10) + 0);
-}
-b++;
-}
-putchar(n);
-a++;
-}
-}
-} 
-}
+	if (n >= 0 && n < 15)
+	{
+		for (a = 0; a <=  n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b; d = c / 100; e = (c /  10) % 10;
+				f = (c % 100) % 10;
+				if (b == 0)
+				{
+					_putchar('0');
+				}
+				else if (c < 10)
+				{
+					_putchar(' '); _putchar(' ');
+					_putchar('0' + f);
+				}
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + e); _putchar('0' + f);
+				}
+				else
+				{
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
+				}
+				if (b < n)
+				{
+					_putchar(','); _putchar(' ');
+				}
+				else
+					_putchar('\n');
+			}
+		}
+	}
