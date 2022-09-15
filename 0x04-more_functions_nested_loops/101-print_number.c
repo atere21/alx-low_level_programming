8c5b359af2d0ec6e1e4dc1a3f9878faeb3d02602
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 21:52:29 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,46 @@
 #include "main.h"
+
 /**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * print_number -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
  */
 void print_number(int n)
 {
+	unsigned int z;
+	int m, b;
 
-	return (0);
+	b = 10;
+
+	if (n < 10 && n >= 0)
+	{
+		putchar (n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n - 2 * n;
+		putchar('-');
+		putchar (n + '0');
+	}
+
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			putchar ('-');
+		}
+		z = n;
+	while (z / b > 9)
+	{
+		b = b * 10;
+	}
+	while (b > 0)
+	{
+		m = z / b;
+		z = z % b;
+		 putchar (m + '0');
+		b = b / 10;
+	}
+	}
 }
