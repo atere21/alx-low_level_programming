f385fedb2cc7cf21b74e2f98514cbb896008ded4
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 22:01:34 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,91 @@
-#include <stdio.h>
+#include "main.h"
+#include <limits.h>
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
- */
+* print_number - outputs a number
+* @n: an int given from main
+*
+* Description: Uses headers to link and a nested loops to achieve goal
+* Return: no return is void
+* A: The case when n is zero
+* B: I set places to be 1 billion initially. If it was bigger than the 
+* number then I reduce places by a factor of 10 until it is small 
+* enough to do nice math.
+* C: Once places has shrunk to match the number, we use math to get 
+* and print the letters. The math here focuses on getting the last letter
+* using mod but it will mod a very specific number. That last number will
+* range from the first and last digits because we divide the number by
+* places and each iteration we decrease places by x10. EG. so 420515 will
+* first be divided by 1000000 which gives 4 and if we mod that by 10
+* we get 4. Next we reduce places so it is 420515 / 100000 which gives
+* 42 and we mod 10 which gives 2. Repeat again with 420515 / 1000 to get
+* the 0, with 100 to get the 5, 10 to get the 1, and now we can just mod
+* 10 and get the last 5.
+* D:I basically cheated on the no hard coding requirement. I used a 
+* limits.h library and did do if statements for the max and min numbers
+* of int. My code worked fine for all numbers except those two. I needed to
+* put it into a loop and break because the PS2 kept showing.
+* E: If the number is negative I will convert it to a positive
+* and print a minus sign. Then proceed with the code as normal.
+*/
+
+
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	int x = n;
+	int digit;
+	int places = 1000000000;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)/* E */
 	{
-		putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
+		x = -n;
 		putchar('-');
-		putchar (n + '0');
 	}
-
+	if (n == INT_MAX || n == INT_MIN) /* D */
+	{
+		while (1)
+		{
+			if (n == INT_MAX)
+			{
+				 putchar('2');
+				 putchar('1');
+				 putchar('4');
+				 putchar('7');
+				 putchar('4');
+				 putchar('8');
+				 putchar('3');
+				 putchar('6');
+				 putchar('4');
+				 putchar('7');
+				break;
+			}
+			else if (n == INT_MIN)
+			{
+				 putchar('2');
+				 putchar('1');
+				 putchar('4');
+				 putchar('7');
+				 putchar('4');
+				 putchar('8');
+				 putchar('3');
+				 putchar('6');
+				 putchar('4');
+				 putchar('8');
+				break;
+			}
+		}
+	}
+	else if (n == 0)/* A */
+		putchar('0');
 	else
 	{
-		if (n < 0)
+		while (places > x)/* B */
+			places /= 10;
+		while (places > 0)
 		{
-			n = n * -1;
-			putchar ('-');
+			digit = x / places;/* C */
+			putchar((digit % 10) + '0');
+			places /= 10;
 		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		 putchar (m + '0');
-		b = b / 10;
-	}
 	}
 }
