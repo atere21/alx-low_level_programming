11f2b581c8e46255dbdff980112bc539129a62bc
atere21 <tosinateret@gmail.com>
2022-09-15 00:25:51 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+
+
+int _isupper(int c)
+{
+0-isupper.c if (c >= A && c <= Z)
+0-isupper.c 0-isupper.c return (1);
+0-isupper.c else
+0-isupper.c 0-isupper.c return (0);
+}
