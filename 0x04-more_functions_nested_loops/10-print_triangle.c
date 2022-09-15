85f9ac3e954066be0d6e83d517f2bce4b547ada8
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 21:06:50 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,17 +13,17 @@ void print_triangle(int size)
 	int a, b;
 
 	if (size <= 0)
-		_putchar('\n');
+		putchar('\n');
 	for (a = 1; a <= size; a++)
 	{
 		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
-				_putchar(' ');
+				putchar(' ');
 			else
-				_putchar('#');
+				putchar('#');
 		}
-		_putchar('\n');
+		 putchar('\n');
 	}
 
 }
