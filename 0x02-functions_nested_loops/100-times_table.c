d6037cb0f6bdb672a5f616fb3ac1bcb648b08ab3
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 05:09:27 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
- */
+* print_times_table - print times tables from zero to n
+* @n: integer
+*/
 
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+int a = 0;
 
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b; d = c / 100; e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					putchar('0');
-				}
-				else if (c < 10)
-				{
-					putchar(' '); putchar(' ');
-					putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					putchar(' ');
-					putchar('0' + e); putchar('0' + f);
-				}
-				else
-				{
-					putchar('0' + d);
-					putchar('0' + e);
-					putchar('0' + f);
-				}
-				if (b < n)
-				{
-					putchar(','); putchar(' ');
-				}
-				else
-					putchar('\n');
-			}
-		}
-	}
+if (n >= 0 && n <= 15)
+{
+while (a <= n)
+{
+int b = 1;
+
+_putchar('0');
+while (b <= n)
+{
+int p = a * b;
+
+_putchar(',');
+_putchar(' ');
+if (p < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(p + '0');
+}
+else if (p > 99)
+{
+_putchar((p / 100) + '0');
+_putchar(((p / 10) % 10) + '0');
+_putchar((p % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
+}
+} 
+}
