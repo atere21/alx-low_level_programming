196aaf149019fe37776a41f933c9be57d278775c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 12:40:41 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
+ * print_numbers - prints 0123456789\n
  *
- * Return: 0-9 followed by newline
+ * Return: void
  */
+
+
 void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
