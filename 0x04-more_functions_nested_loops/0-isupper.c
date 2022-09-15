46ff3d27e6d3081ed74f8405254b0029eb0f4e1a
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-15 12:30:29 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,25 +1,14 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
- * Description: Prints Holberton with _putchar
- *
- * Return: always 0
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
-
 {
-	int i = 0;
-	char c;
-	char s[] = "Holberton\n";
-
-	while (i <= 9)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
-	return (0);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
