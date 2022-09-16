0f78e0099d9d96c128cf729dd9c81ea0df27eaa1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 17:35:23 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,19 +9,13 @@
 void print_number(int n)
 {
 unsigned int k = n;
-
-if (n < 0)
- 
+if (n < 0) 
 {
 n *= -1;
 k = n;
 }
-
 k /= 10;
-
 if (k != 0)
 print_number(k);
-
 putchar((unsigned int) n % 10 + 0);
-
 }
