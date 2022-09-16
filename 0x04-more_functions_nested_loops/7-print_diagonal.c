3231540dafb2764e9ece2dc7cf24e3048b1fef43
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:24:10 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_line - a function that draws a straight line in the terminal
- * @n: input number of times to print _
- * Return: a straight line
- */
-void print_line(int n)
-{
-int co;
+* print_diagonal - prints a diagonal line with new line
+* @n: an int given by main
+*
+* Description: Uses headers to link and a nested loops to achieve goal
+* Return: void. no return.
+*/
 
-if (n <= 0)
+void print_diagonal(int n)
 {
-putchar(n);
-}
-else
-{
-for (co = 1; co <= n; co++)
-{
-putchar(_);
-}
-putchar(n);
-}
+	int i, j;
+
+	if (n <= 0)/* if 0 or negative just newline */
+		putchar('\n');
+	for (i = 0; i < n; i++)/* we want the amount of rows equal to n */
+	{
+	/*for each column we start at the origin 0,0*/
+	/* when i did j = +i i get a different pattern, the up right pattern*/
+	/* i is the inital spacing needed eg. for 10*/
+	/* j is the counter. j = 10 - 0 , 10 is not less than 10, so skip*/
+	/* now j = 9, 9 < 10 true, do once */
+	/* omg i am a genius UPDATENVM. im an idiot. i could have  */
+	/* just done a i = j check and insert \ if true and space if not*/
+		for (j = n - i; j < n; j++)
+		{
+			putchar(' ');
+		}
+		 putchar('\\');
+		 putchar('\n');
+	}
 }
