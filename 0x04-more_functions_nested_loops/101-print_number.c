792b0df7a1c6acf6e26ee14bb201043d08fd1883
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 06:05:48 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - prints an integer
@@ -11,7 +11,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('-');
+		putchar('-');
 	} else
 	{
 		n1 = n;
@@ -22,5 +22,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	 putchar((n1 % 10) + '0');
 }
