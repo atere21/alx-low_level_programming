aa7ac9d76e7956a0c292ae322ceea27ee93c0cfd
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:47:59 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,11 @@
 
 void print_line(int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n; i++)
-	{
-           putchar('_');
-	}
-	 putchar('\n');
+for (i = 0; i < n; i++)
+{
+putchar('_');
+}
+putchar('\n');
 }
