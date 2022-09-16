6cbe87b6acfb4095a82c265c2611b1adfd528bed
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 05:09:36 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,12 @@ void more_numbers(void)
 		{
 			if (y > 9)
 			{
-				_putchar((y / 10) + '0');
+				putchar((y / 10) + '0');
 			}
 
-			_putchar((y % 10) + '0');
+			         putchar((y % 10) + '0');
 		}
 
-		_putchar('\n');
+		                  putchar('\n');
 	}
 }
