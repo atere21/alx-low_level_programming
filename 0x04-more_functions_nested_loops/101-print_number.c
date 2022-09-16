7f7ecf9a2bbae65dc9df9e7f47ea409dae3a1b7a
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:33:50 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,28 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - a function that prints an integer.
- *
- * Return: Always 0.
- */
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
+void print_number(int n)
+{
+	unsigned int k = n;
 
-int main() 
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		 putchar('-');
+	}
 
-{   
-    int number;
-   
-      putchar("Enter an integer: ");  
-    
-    // reads and stores input
-    scanf("%d", &number);
+	k /= 10;
 
-    // displays output
-    putchar("You entered: %d", number);
-    
-    return 0;
+	if (k != 0)
+		print_number(k);
+
+	 putchar((unsigned int) n % 10 + '0');
+
 }
