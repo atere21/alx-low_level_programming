45ca702f01784580bbbb98d25737d7aadbcc19f1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:26:11 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
-* print_diagonal - prints a diagonal line with new line
-* @n: an int given by main
-*
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: void. no return.
-*/
-
+ * print_diagonal -  a function that draws a diagonal line on the terminal
+ * @n: input number of times '\' should be printed
+ * Return: a diagonal
+ */
 void print_diagonal(int n)
 {
-	int i, j;
+	int co, sp;
 
-	if (n <= 0)/* if 0 or negative just newline */
-		putchar('\n');
-	for (i = 0; i < n; i++)/* we want the amount of rows equal to n */
+	if (n <= 0)
 	{
-	/*for each column we start at the origin 0,0*/
-	/* when i did j = +i i get a different pattern, the up right pattern*/
-	/* i is the inital spacing needed eg. for 10*/
-	/* j is the counter. j = 10 - 0 , 10 is not less than 10, so skip*/
-	/* now j = 9, 9 < 10 true, do once */
-	/* omg i am a genius UPDATENVM. im an idiot. i could have  */
-	/* just done a i = j check and insert \ if true and space if not*/
-		for (j = n - i; j < n; j++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
 		{
-			putchar(' ');
+			for (sp = 1; sp < co; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		 putchar('\\');
-		 putchar('\n');
 	}
 }
