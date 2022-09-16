1c504820dae1d5d0b59c0a6e3a3a0accf23fb5d1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:19:37 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,69 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* print_number - outputs a number
-* @n: an int given from main
-*
-*Then proceed with the code as normal.
-*/
+ * main - a function that prints an integer.
+ *
+ * Return: Always 0.
+ */
 
+int main() 
 
-void print_number(int n)
-{
-	int x = n;
-	int digit;
-	int places = 1000000000;
+{   
+    int number;
+   
+      putchar("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
 
-	if (n < 0)/* E */
-	{
-		x = -n;
-		putchar('-');
-	}
-	if (n == INT_MAX || n == INT_MIN) /* D */
-	{
-		while (1)
-		{
-			if (n == INT_MAX)
-			{
-				 putchar('2');
-				 putchar('1');
-				 putchar('4');
-				 putchar('7');
-				 putchar('4');
-				 putchar('8');
-				 putchar('3');
-				 putchar('6');
-				 putchar('4');
-				 putchar('7');
-				break;
-			}
-			else if (n == INT_MIN)
-			{
-				 putchar('2');
-				 putchar('1');
-				 putchar('4');
-				 putchar('7');
-				 putchar('4');
-				 putchar('8');
-				 putchar('3');
-				 putchar('6');
-				 putchar('4');
-				 putchar('8');
-				break;
-			}
-		}
-	}
-	else if (n == 0)/* A */
-		putchar('0');
-	else
-	{
-		while (places > x)/* B */
-			places /= 10;
-		while (places > 0)
-		{
-			digit = x / places;/* C */
-			putchar((digit % 10) + '0');
-			places /= 10;
-		}
-	}
+    // displays output
+    putchar("You entered: %d", number);
+    
+    return 0;
 }
