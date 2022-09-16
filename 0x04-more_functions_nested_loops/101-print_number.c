9ee612bfc535171f41c0836af11a0d3b4e954c93
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-16 04:44:01 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-
 /**
-  * print_number - Prints an integer.
-  * @n: The integer to prints.
-  *
-  * Return: Nothing!
-  */
-void print_number(int n)
+ * print_line - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * co represents no of count
+ *
+ * Return: a straight line
+ */
+void print_line(int n)
 {
-unsigned int k = n;
+	int co;
 
-if (n < 0)
-{
-n *= -1;
-k = n;
-putchar('-');
-}
-
-k /= 10;
-
-if (k != 0)
-
-putchar(k);
-putchar((unsigned int) n % 10 + '0');
-
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			putchar('_');
+		}
+		 putchar('\n');
+	}
 }
