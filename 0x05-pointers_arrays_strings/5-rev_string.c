2e531341033cad8382f8308ea6bb0d4cb4e8d63f
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-19 16:21:06 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,24 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * rev_string - a function that reverses a string
- *
  * @s: input to reverse
- * Return: void
+ * Return: string and in reverse
  */
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	int i = 0, j = 0;
-	char str[500];
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
 
-	while (*(s + i))
+	while (s[fcounter] != '\0')
+		fcounter++;
+
+	for (i = 0; i < fcounter; i++)
 	{
-		*(str + i) = *(s + i);
-		i++;
-	}
-	i = i - 1;
-	while (i >=0)
-	{
-		*(s + i) = *(str + j);
-		j++;
-		i--;
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
 	}
 }
-
