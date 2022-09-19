fbde6102346657fda6ba4961036f9be7b641d4ef
atere21 <tosinateret@gmail.com>
2022-09-19 08:14:22 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that revers
+ * es a string 
+ *
+ * Return: void
+ */
+void rev_string(char *s);
+{
+	int i = 0, j = 0;
+	char str[500];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >=0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
+
