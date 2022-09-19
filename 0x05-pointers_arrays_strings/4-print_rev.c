163e9060d3181e2fe369d6c66a5014aa81b4c5d8
atere21 <tosinateret@gmail.com>
2022-09-19 10:01:58 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,8 +15,29 @@ i++;
 i = i - 1;
 while (i >= 0)
 {
+_putchar(*(s + i));
+i--;
+}
+_putchar(n);
+}#include "holberton.h"
+
+/**
+ * print_rev - prints a string in stdout in reverse
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0;
+
+while (*(s + i))
+i++;
+i = i - 1;
+while (i >= 0)
+{
 putchar(*(s + i));
 i--;
 }
-putchar('\n');
+putchar(n);
 }
