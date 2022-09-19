b152fcc62b711cafbe24dee06de171add73508f4
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-19 13:14:37 +0100

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in stdout in reverse
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		putchar(*(s + i));
+		i--;
+	}
+	putchar('\n');
+}
