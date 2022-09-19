b0cb840d03d275900198d10c00f3e2fdcb124604
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-19 14:27:33 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
  */
 void print_rev(char *s)
 {
-       int i = 0;
+	int i = 0;
 
 	while (*(s + i))
 		i++;
 	i = i - 1;
 	while (i >= 0)
 	{
-	putchar(*(s + i));
-	i--;
+		putchar(*(s + i));
+		i--;
 	}
 	putchar('\n');
 }
