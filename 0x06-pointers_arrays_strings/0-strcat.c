a2c66bb9def54564d1add01e27d42a302dbad7d7
atere21 <tosinateret@gmail.com>
2022-09-21 03:13:19 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,39 +1,23 @@
-#include "main .h"
+#include "main.h"
 
 /**
-<<<<<<< HEAD
- * 1-strncat - a function that concatenates two strings
- * @dest: copy to
- * @src: copy from
- * @n: input of max bytes to be used
- * Return: Always 0 (Success)
-=======
- * 1-strcat - appends src to the dest
- * string
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  *
  * Return: address of dest
->>>>>>> 2419d182440d2654c4c7ebcd61601f855aee973d
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-<<<<<<< HEAD
-int i, co;
-=======
-	int i, j;
->>>>>>> 2419d182440d2654c4c7ebcd61601f855aee973d
+int i, j;
 
-for (i = 0; dest[i] != 0; i++)
+i = j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
 {
+i++;
+j++;
 }
-
-for (co = 0; co < n; co++)
-{
-dest[i + co] = src[co];
-if (src[co] == 0)
-co = n;
-}
-
 return (dest);
 }
