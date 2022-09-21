d64e045f569541d1208f8b53e227add359132e8b
atere21 <tosinateret@gmail.com>
2022-09-21 05:35:05 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses the content of an array of int
+ * @a: content
+ * @n: elements of content
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+int i, temp;
+
+for (i = 0; i < n; i++)
+{
+n--;
+temp = a[i];
+a[i] = a[n];
+a[n] = temp;
+}
+}
