a8651fcedac4e3329dee69d92e1758f216ae5788
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-21 10:36:15 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,10 +4,10 @@
 /**
  * 1-strncat - appends src to the dest string
  * @dest: string to append by src
- * @src: string to append to dest
+ * @src: string to append to *dest
  * @n: largest number of bytes to append
  *
- * Return: address of dest
+ * Return: address of *dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
