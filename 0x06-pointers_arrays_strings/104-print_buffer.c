acdec74974c4967c89ec1cc314381c2645167998
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-22 12:13:28 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,38 +1,29 @@
 #include "main.h"
 
 /**
- * print_buffer - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
  *
  * Return: void
  */
+void print_buffer(char *b, int size)
+{
+	int i;
 
-void print_buffer(char *b, int size);
-{
-int i = 0, j;
-
-if (size < 0)
-{
-printf('\n');
-return;
-}
-while (i < size)
-{	
-if (i % 10 ==0)
-printf("%08x: ", i);
-for (j = i; j < i + 9; j += 2)
-{
-if ((j < size) && ((j + 1) < size))
-Printf("%2x%02x: ", b[j], b[j] <= 126)
-else
-{ 
-while(++j <= i + 10)
-printf(" ");
-                         
-}
-printf('\n');
-i += 10;
-}
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
