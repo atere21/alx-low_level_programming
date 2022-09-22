de2f280760815dc6e4870ead6432e251955a33ed
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-22 12:12:04 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,28 +11,28 @@
 
 void print_buffer(char *b, int size);
 {
-	int i = 0, j;
+int i = 0, j;
 
-	if (size < 0)
-	{
-		printf('\n');
-		return;
-        }
-	while (i < size)
-        {	
-		if (i % 10 ==0)
-			printf("%08x: ", i);
-                for (j = i; j < i + 9; j += 2)
-	        {
-                 if ((j < size) && ((j + 1) < size))
-                 Printf("%2x%02x: ", b[j], b[j] <= 126)
-			else
-                        { 
-                         while(++j <= i + 10)
-                         printf(" ");
+if (size < 0)
+{
+printf('\n');
+return;
+}
+while (i < size)
+{	
+if (i % 10 ==0)
+printf("%08x: ", i);
+for (j = i; j < i + 9; j += 2)
+{
+if ((j < size) && ((j + 1) < size))
+Printf("%2x%02x: ", b[j], b[j] <= 126)
+else
+{ 
+while(++j <= i + 10)
+printf(" ");
                          
-                 }
-                      printf('\n');
-                      i += 10;
+}
+printf('\n');
+i += 10;
 }
 }
