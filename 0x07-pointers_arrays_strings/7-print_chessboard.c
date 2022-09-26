2b50391fe7c579e93fde243b3bb335f1317e25ca
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-26 13:45:29 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,13 +10,13 @@ void print_chessboard(char (*a)[8])
 {
 	int b, c;
 
-	for (b = 0; b < 8; b++)
+	for (b = 0; b <= 8; b++)
 	{
-		for (c = 0; c < 8; c++)
+		for (c = 0; c <= 8; c++)
 		{
 			_putchar(a[b][c]);
 		}
 
-		_putchar('\n');
+		_putchar(10);
 	}
 }
