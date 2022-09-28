5bab82e833066b43ae36d8fedcd54da3f7f0d3a4
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-28 10:31:04 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line
@@ -10,7 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
