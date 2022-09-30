0cce83bfeb319c386814299622a1f857d3b50301
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-30 12:12:48 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,57 +2,32 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
 /**
- * _isnumber - checks if string is a number
- * @s: string
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * Return: On success 1.
- * If not a number, 0 is returned.
+ * Return: 0 if no errors, else 1
  */
-int _isnumber(char *s)
+int main(int argc, char *argv[])
 {
-int i, check, d;
+	int a = 0, i, j;
 
-i = 0, d = 0, check = 1;
-if (*s == -)
-i++;
-for (; *(s + i) != 0; i++)
-{
-d = isdigit(*(s + i));
-if (d == 0)
-{
-check = 0;
-break;
-}
-}
-return (check);
-}
-/**
- * main - Entry point
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
- */
-int main(int argc, char **argv)
-{
-int i, n, ex;
-
-ex = 0, n = 0;
-if (argc > 1)
-{
-for (i = 1; i < argc; i++)
-{
-if (_isnumber(argv[i]))
-n += atoi(argv[i]);
-else
-ex = 1;
-}
-}
-if (ex == 0)
-printf("%i\n", n);
-else
-printf("%s\n", "Error");
-return (ex);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
