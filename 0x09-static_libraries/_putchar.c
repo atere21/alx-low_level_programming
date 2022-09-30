bf98089c0dca3b556c1d006c553ac046e708b6c9
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-09-30 19:32:04 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,10 +3,11 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int putchar(char c)
+int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
