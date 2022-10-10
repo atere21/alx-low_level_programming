204a1f1ed19cb0e8315bdb75dde17d6fe0d88ee2
atere21 <tosinateret@gmail.com>
2022-10-10 04:30:32 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - writes the character c to stdout
+ * @d: Dog
+ * @name: Dog name
+ * @age: dog age
+ * @owner: Dog owner
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != 0)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
