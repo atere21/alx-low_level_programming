6f2a1b2a1dd32b1dd045bcd2743d038d8b13384b
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-12 12:13:21 +0100

    Update main.h

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,4 +1,4 @@
-#ifndef function_pointers.h
+lol#ifndef function_pointers.h
 #define function_pointers.h
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
