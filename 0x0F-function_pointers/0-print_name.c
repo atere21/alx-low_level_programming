e9bf8f0046059c8a7d33f596169722776a4aecc7
atere21 <tosinateret@gmail.com>
2022-10-12 04:07:07 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - writes the character c to stdout
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (f != 0)
+f(name);
+}
