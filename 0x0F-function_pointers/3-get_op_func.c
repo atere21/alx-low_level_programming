3fa92ee65846577327a112322cc510fe31fad26a
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-12 17:37:07 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,29 @@
-#include "3-calc.h"
-#include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
-
+#include "3-calc.h"
 /**
- * get_op_func - Calls function to perform arithmetic operation
- * between 2 numbers bases on an operator
- * @s: operator passed as argument to the program
- *
- * Return: Nothing
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-int i;
+	int i = 0;
 
-i = 0;
-while ((ops[i].op[0] != s[0]) && (i < 5))
-i++;
-
-return (ops[i].f);
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
 }
