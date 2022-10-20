e5b891c91ab27ba8695214d1e79cc6044bf3c199
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-20 16:06:54 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include <stdio.h>
 
 void msg(void)__attribute__((constructor));
@@ -14,4 +12,3 @@ void msg(void)
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
 }
->>>>>>> 918bb3e76b991fac8800c818781d4cfd8599d1b5
