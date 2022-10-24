42a00f957ea4ed5bb747f05ef3fc95192e9a08da
atere21 <tosinateret@gmail.com>
2022-10-24 04:21:20 -0700

    Task 5

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,4 @@
-#include lists.h
+#include "lists.h"
 #include <stdlib.h>
 
 /**
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees memory of a list
+ * @head: pointer to head of singly linked list
+ *
+ * Return: No Return
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head)
+{
+while (*head != 0)
+{
+tmp = *head;
+*head = tmp->next;
+free(tmp);
+}
+}
+
+}
