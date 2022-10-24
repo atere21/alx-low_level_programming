abb7f720b2959eca05bf2224928974a74ea7e05e
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-24 19:20:23 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,17 +9,17 @@
  */
 int pop_listint(listint_t **head)
 {
-listint_t *tmp = *head;
-listint_t *tmp2;
-int n;
+	listint_t *tmp = *head;
+	listint_t *tmp2;
+	int n;
 
-if (head == 0 || tmp == 0)
-return (0);
+	if (head == 0 || tmp == 0)
+		return (0);
 
-tmp2 = tmp->next;
-n = tmp->n;
-free(tmp);
-*head = tmp2;
+	tmp2 = tmp->next;
+	n = tmp->n;
+	free(tmp);
+	*head = tmp2;
 
-return (n);
+	return (n);
 }
