e328db56f5c3936fd3982187f02bc675920c169c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-24 18:56:23 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -17,6 +19,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,7 +32,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
-#endif
+
+#endif /* LISTS_H */
