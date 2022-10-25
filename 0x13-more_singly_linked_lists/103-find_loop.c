8daca91e82bb5398c907c292c1e94034371adbaa
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-25 04:57:20 +0100

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,21 +8,27 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *slow = head;
-listint_t *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
- (NULL);
+	if (!head)
+		return (NULL);
 
-while (slow && fast && fast->next)
-{
-= fast->next->next;
-= slow->next;
-(fast == slow)
-= head;
-(slow != fast)
-= slow->next;
-= fast->next;
- (fast);
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
 
-return (NULL);
+	return (NULL);
 }
