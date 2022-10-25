25ea0b98d503b522d0bb3d779a77db743000f8c3
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-25 04:38:43 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,23 +8,31 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t len = 0;
-int diff;
-listint_t *temp;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-(0);
+	if (!h || !*h)
+		return (0);
 
-while (*h)
-{
-= *h - (*h)->next;
-(diff > 0)
-= (*h)->next;
-= temp;
-++;
-= NULL;
-++;
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
 
-*h = NULL;
+	*h = NULL;
 
-return (len);
+	return (len);
 }
