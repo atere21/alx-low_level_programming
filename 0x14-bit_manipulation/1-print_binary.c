8a7661d5c926d49b759ee4d6b0f883b91fffcba8
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-10-27 17:16:20 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,34 +1,44 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
+ * rev_bin - prints binary representation of number with & and Shift Right
+ * operators, but uses recursion to reverse the result
+ *
+ * @n: decimal number
+ * @check: checks for special case number 0
+ *
+ * Return: No return
  */
-void print_binary(unsigned long int n)
+void rev_bin(unsigned long int n, int check)
 {
-	int i = 0, count, k, temp;
-
-	if (n == 0)
+	if (check == 1)
 	{
-		printf("0");
+		putchar('0');
 		return;
 	}
 
-	temp = n;
+	if (n == 0)
+		return;
 
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
+	rev_bin(n >> 1, check);
 
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	if ((n & 1) == 0)
+		putchar('0');
+
+	if ((n & 1) == 1)
+		putchar('1');
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
+ *
+ * Return: No return
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		rev_bin(n, 1);
+	else
+		rev_bin(n, 0);
 }
