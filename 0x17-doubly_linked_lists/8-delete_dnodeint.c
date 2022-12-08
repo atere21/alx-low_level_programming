b00275a88ece6dce42661815c552aba97787c4d6
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-08 19:08:27 +0100

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,4 @@
-#include lists.h
+#include "lists.h"
 
 /**
  * delete_dnodeint_at_index - deletes the node at index of a
@@ -10,37 +10,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *h1;
-dlistint_t *h2;
-unsigned int i;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-h1 = *head;
+	h1 = *head;
 
-if (h1 != NULL)
-while (h1->prev != NULL)
-h1 = h1->prev;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-i = 0;
+	i = 0;
 
-while (h1 != NULL)
-{
-if (i == index)
-{
-if (i == 0)
-{
-*head = h1->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-}
-else
-{
-h2->next = h1->next;
+	while (h1 != NULL)
+	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
 
-if (h1->next != NULL)
-ext->prev = h2;
- (1);
-= h1;
-= h1->next;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
 
-return (-1);
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
+	}
+
+	return (-1);
 }
