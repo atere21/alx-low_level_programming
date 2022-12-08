0324034fe4e3eec75cc3ba095137e1d2c481b17c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-08 17:27:42 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,4 @@
-#include lists.h
+#include "lists.h"
 
 /**
  * print_dlistint - prints all the elements of a
@@ -9,22 +9,22 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int count;
+	int count;
 
-count = 0;
+	count = 0;
 
-if (h == NULL)
-return (count);
+	if (h == NULL)
+		return (count);
 
-while (h->prev != NULL)
-h = h->prev;
+	while (h->prev != NULL)
+		h = h->prev;
 
-while (h != NULL)
-{
-printf(%dn, h->n);
-count++;
-h = h->next;
-}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
 
-return (count);
+	return (count);
 }
