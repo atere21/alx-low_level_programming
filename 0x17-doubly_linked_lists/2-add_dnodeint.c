c4012eaf5d62a464fd635cb759360c3d0a274576
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-08 17:24:48 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,4 @@
-#include lists.h
+include "lists.h"
 
 /**
  * add_dnodeint - adds a new node at the beginning
@@ -10,29 +10,29 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
-dlistint_t *h;
+	dlistint_t *new;
+	dlistint_t *h;
 
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
-return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-new->n = n;
-new->prev = NULL;
-h = *head;
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
 
-if (h != NULL)
-{
-while (h->prev != NULL)
-h = h->prev;
-}
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
 
-new->next = h;
+	new->next = h;
 
-if (h != NULL)
-h->prev = new;
+	if (h != NULL)
+		h->prev = new;
 
-*head = new;
+	*head = new;
 
-return (new);
+	return (new);
 }
