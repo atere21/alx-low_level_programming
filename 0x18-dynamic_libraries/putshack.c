dbe1c3da1badbe6d0d80440c465bf56551e9ce26
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-12-12 17:55:43 +0100

    Update putshack.c

--- 0x18-dynamic_libraries/putshack.c
+++ 0x18-dynamic_libraries/putshack.c
@@ -3,20 +3,20 @@
 
 int rand()
 {
-		static int ct = -1;
+	static int ct = -1;
 
-			ct++;
-				if (ct == 0)
-							return 8;
-					if (ct == 1)
-								return 8;
-						if (ct == 2)
-									return 7;
-							if (ct == 3)
-										return 9;
-								if (ct == 4)
-											return 23;
-									if (ct == 5)
-												return 74;
-										return ct * ct % 30000;
+	ct++;
+	if (ct == 0)
+		return 8;
+	if (ct == 1)
+		return 8;
+	if (ct == 2)
+		return 7;
+	if (ct == 3)
+		return 9;
+	if (ct == 4)
+		return 23;
+	if (ct == 5)
+		return 74;
+	return ct * ct % 30000;
 }
